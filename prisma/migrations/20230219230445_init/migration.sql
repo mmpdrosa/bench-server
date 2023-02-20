036ad6b30938b08c637f44cfaf9a934f44fc14dc
mmpdrosa <matheusmp2001@gmail.com>
2023-02-20 00:22:23 -0300

    add migrations

---
+++ prisma/migrations/20230219230445_init/migration.sql
@@ -0,0 +1,183 @@
+-- CreateTable
+CREATE TABLE "retailers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "html_url" TEXT NOT NULL,
+
+    CONSTRAINT "retailers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "reference_price" INTEGER,
+    "image_url" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "coupons" (
+    "id" TEXT NOT NULL,
+    "available" BOOLEAN NOT NULL,
+    "code" TEXT NOT NULL,
+    "discount" TEXT NOT NULL,
+    "minimum_spend" INTEGER,
+    "store" TEXT,
+    "description" TEXT,
+    "retailer_id" TEXT NOT NULL,
+
+    CONSTRAINT "coupons_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products_retailers" (
+    "id" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "store" TEXT,
+    "available" BOOLEAN NOT NULL,
+    "html_url" TEXT NOT NULL,
+    "dummy" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "retailer_id" TEXT NOT NULL,
+    "coupon_id" TEXT,
+
+    CONSTRAINT "products_retailers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subcategories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+
+    CONSTRAINT "subcategories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products_categories" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+
+    CONSTRAINT "products_categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products_subcategories" (
+    "id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "subcategory_id" TEXT NOT NULL,
+
+    CONSTRAINT "products_subcategories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "prices_history" (
+    "id" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "product_id" TEXT NOT NULL,
+
+    CONSTRAINT "prices_history_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscriptions" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "endpoint" TEXT NOT NULL,
+    "keys" JSONB NOT NULL,
+
+    CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users_products_notifications" (
+    "id" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+
+    CONSTRAINT "users_products_notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users_categories_notifications" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+
+    CONSTRAINT "users_categories_notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "retailers_name_key" ON "retailers"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "coupons_code_retailer_id_key" ON "coupons"("code", "retailer_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_retailers_product_id_retailer_id_key" ON "products_retailers"("product_id", "retailer_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subcategories_category_id_name_key" ON "subcategories"("category_id", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_categories_product_id_key" ON "products_categories"("product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_subcategories_product_id_key" ON "products_subcategories"("product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions_user_id_endpoint_key" ON "subscriptions"("user_id", "endpoint");
+
+-- AddForeignKey
+ALTER TABLE "coupons" ADD CONSTRAINT "coupons_retailer_id_fkey" FOREIGN KEY ("retailer_id") REFERENCES "retailers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_retailers" ADD CONSTRAINT "products_retailers_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_retailers" ADD CONSTRAINT "products_retailers_retailer_id_fkey" FOREIGN KEY ("retailer_id") REFERENCES "retailers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_retailers" ADD CONSTRAINT "products_retailers_coupon_id_fkey" FOREIGN KEY ("coupon_id") REFERENCES "coupons"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subcategories" ADD CONSTRAINT "subcategories_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_categories" ADD CONSTRAINT "products_categories_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_categories" ADD CONSTRAINT "products_categories_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_subcategories" ADD CONSTRAINT "products_subcategories_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products_subcategories" ADD CONSTRAINT "products_subcategories_subcategory_id_fkey" FOREIGN KEY ("subcategory_id") REFERENCES "subcategories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "prices_history" ADD CONSTRAINT "prices_history_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users_products_notifications" ADD CONSTRAINT "users_products_notifications_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users_categories_notifications" ADD CONSTRAINT "users_categories_notifications_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
